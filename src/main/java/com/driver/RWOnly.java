2c93295f7861f24b7a8ae130aa3411628f67fc9d
Kowshik1212 <mkowshikreddy@gmail.com>
2024-01-11 13:41:47 +0530

    encapsulation is shown

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    void setter(String name){
+        this.name=name;
+    }
+    String getter(){
+        return this.name;
+    }
 }
