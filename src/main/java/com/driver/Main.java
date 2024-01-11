f5609eac431aa9b6119f871fcda20be0f56f600d
Kowshik1212 <mkowshikreddy@gmail.com>
2024-01-11 13:46:15 +0530

    renamed getter and setter functions

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
   public static void main(String[] args){
       RWOnly obj=new RWOnly();
-      obj.setter("Kowshik");
-      System.out.println(obj.getter());
+      obj.setName("Kowshik");
+      System.out.println(obj.getName());
   }
 }
